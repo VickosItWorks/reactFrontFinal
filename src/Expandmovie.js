480e3d55c971dcc789e438638bb75089c6a377c8
Joaquin Nader <joaquinnader@gmail.com>
2023-08-05 18:05:24 -0300

    fix(useUserData): fixed weird behavior

--- src/Expandmovie.js
+++ src/Expandmovie.js
@@ -7,26 +7,26 @@ import { styled } from "styled-components";
 import { useUserData } from "./useUserData";
 
 const Boton = styled.button`
-        border: none;
-        background-color: #370b5a;
-        cursor: pointer;
-        
-        margin-right: 10px;
-        padding: 10px;
-        border: none;
-        border-radius: 4px;
-        color: white
-    `
+    border: none;
+    background-color: #370b5a;
+    cursor: pointer;
+    
+    margin-right: 10px;
+    padding: 10px;
+    border: none;
+    border-radius: 4px;
+    color: white;
+`;
 
 const ExpandedElements = styled.div.attrs({ className: 'expanded-info' })`
-    background-color: #f5f5f5;
-    padding: 10px;
-`
+  background-color: #f5f5f5;
+  padding: 10px;
+`;
 
 const Expandmovie = () => {
   const [movieDetails, setMovieDetails] = useState(null);
   const params = useParams();
-  const {userId, favorites, watchList} = useUserData();
+  const { favorites, updateFavorites, watchList, updateWatchList } = useUserData(movieDetails);
 
   useEffect(() => {
     fetch(`https://www.omdbapi.com/?apikey=9b5184a7&i=${params.imdbID}`)
@@ -42,28 +42,30 @@ const Expandmovie = () => {
 
   const { Rated, Runtime, Genre, Director, Writer, Actors, Plot, Language, Awards } = movieDetails;
 
-  const heartIcon = favorites.includes(movieDetails.imdbID)? 'bi bi-bookmark-heart-fill' : 'bi bi-bookmark-heart';
-  const plusIcon = watchList.includes(movieDetails.imdbID)? 'bi bi-bookmark-plus-fill' : 'bi bi-bookmark-plus';
+  const heartIcon = favorites.includes(movieDetails.imdbID) ? 'bi bi-bookmark-heart-fill' : 'bi bi-bookmark-heart';
+  const plusIcon = watchList.includes(movieDetails.imdbID) ? 'bi bi-bookmark-plus-fill' : 'bi bi-bookmark-plus';
 
-  return (<ExpandedElements className="expanded-info">
-    <p className="description">Rated: {Rated}</p>
-    <p className="description">Runtime: {Runtime}</p>
-    <p className="description">Genre: {Genre}</p>
-    <p className="description">Actors: {Actors}</p>
-    <p className="description">Language: {Language}</p>
-    <p className="description">Awards: {Awards}</p>
-    <p className="description">Director: {Director}</p>
-    <p className="description">Writer: {Writer}</p>
-    <p className="description">Plot: {Plot}</p>
-    <div className="buttons">
-      <Boton className="heart-button" >
-        <i className={heartIcon}></i>
-      </Boton>
-      <Boton className="plus-button" >
-        <i className={plusIcon}></i>
-      </Boton>
-    </div>
-  </ExpandedElements>)
+  return (
+    <ExpandedElements className="expanded-info" onClick={(e) => e.stopPropagation()}>
+      <p className="description">Rated: {Rated}</p>
+      <p className="description">Runtime: {Runtime}</p>
+      <p className="description">Genre: {Genre}</p>
+      <p className="description">Actors: {Actors}</p>
+      <p className="description">Language: {Language}</p>
+      <p className="description">Awards: {Awards}</p>
+      <p className="description">Director: {Director}</p>
+      <p className="description">Writer: {Writer}</p>
+      <p className="description">Plot: {Plot}</p>
+      <div className="buttons">
+        <Boton className="heart-button" onClick={() => updateFavorites(movieDetails.imdbID)}>
+          <i className={heartIcon}></i>
+        </Boton>
+        <Boton className="plus-button" onClick={() => updateWatchList(movieDetails.imdbID)}>
+          <i className={plusIcon}></i>
+        </Boton>
+      </div>
+    </ExpandedElements>
+  );
 }
 
 export default Expandmovie;
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
 import './Home.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Moviewatch from "./Moviewatch";
 import { styled } from "styled-components";
 
@@ -26,7 +26,7 @@ const Home = () => {
         event.preventDefault();
         const isUserLogged = JSON.parse(sessionStorage.getItem('isLogged'));
 
-        if(isUserLogged?.logged){
+        if (isUserLogged?.logged) {
             const inputName = document.getElementById("search");
             setMovieName(inputName.value);
         } else {
@@ -38,18 +38,18 @@ const Home = () => {
         <h1>Movie Database</h1>
 
         <div className="buttons-container">
-                <button className="nav-button" onClick={() => navigate('/user')}>
-                    <i class="bi bi-person-fill"></i>
-                </button>
-                <button className="nav-button" onClick={() => navigate('/login')}>Login</button>
-            </div>
+            <button className="nav-button" onClick={() => navigate('/user')}>
+                <i class="bi bi-person-fill"></i>
+            </button>
+            <button className="nav-button" onClick={() => navigate('/login')}>Login</button>
+        </div>
 
         <form className="search-form">
-            <input type="text" id="search" name="search" placeholder="Search movies..."/>
-            <Largebutton onClick= {movieValue} type="submit">Search</Largebutton>
+            <input type="text" id="search" name="search" placeholder="Search movies..." />
+            <Largebutton onClick={movieValue} type="submit">Search</Largebutton>
         </form>
 
-        <Moviewatch name={movieName}/>
+        <Moviewatch name={movieName} />
 
     </div>)
 }
